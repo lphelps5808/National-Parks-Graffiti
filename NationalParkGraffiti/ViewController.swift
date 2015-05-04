87953200862a1c46cef3ea3c022b720eb44c6bdd
Laura Phelps <laura@tortucalabs.com>
2015-05-03 21:12:19 -0400

    Initial Commit

---
+++ NationalParkGraffiti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NationalParkGraffiti
+//
+//  Created by Laura Phelps on 5/3/15.
+//  Copyright (c) 2015 Laura Phelps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
